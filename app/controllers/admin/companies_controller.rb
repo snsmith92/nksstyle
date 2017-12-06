2a61198ae7965900c3d59a0c1964ec0e2beed21d
snsmith92 <sherzelsmith@gmail.com>
2017-12-06 19:48:40 +0100

    Added ability for company form submit

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -5,4 +5,18 @@ class Admin::CompaniesController < ApplicationController
     @company = Company.new
   end 
 
+  def create
+    @company = current_user.companies.create(company_params)
+    redirect_to admin_company_path(@company)
+  end 
+
+  def show
+    @company = Company.find(params[:id])
+  end 
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description, :address, :phone, :email, :website, :facebook)
+  end 
 end
