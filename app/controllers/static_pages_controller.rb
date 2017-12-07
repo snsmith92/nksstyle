21fbd5b5722d2f85f951559fb400cbe518e406ff
snsmith92 <sherzelsmith@gmail.com>
2017-12-07 23:10:48 +0100

    Made style changes and added random company feed on homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,14 @@
 class StaticPagesController < ApplicationController
 
   def index
+    companies = []
+    companies = Company.order("RANDOM()").limit(6)
+    @company1 = companies[0]
+    @company2 = companies[1]
+    @company3 = companies[2]
+    @company4 = companies[3]
+    @company5 = companies[4]
+    @company6 = companies[5]
   end 
   
 end
--- app/models/servicegroup.rb
+++ app/models/servicegroup.rb
@@ -1,3 +1,5 @@
 class Servicegroup < ApplicationRecord
   belongs_to :company
+
+  validates :title, presence: true, length: {maximum: 50, minimum: 5}
 end
